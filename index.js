421d5c9e1525381830e4ae17cc743172711652f8
GDP-IrvanAriyanto <irvan.ariyanto@gdplabs.id>
2021-06-22 13:50:33 +0700

    Callback Hell and Promises

--- index.js
+++ index.js
@@ -6,37 +6,39 @@ const url = "mongodb://root:rootpassword@localhost:27017/";
 
 const dbname = "conFusion";
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-  assert.equal(err, null);
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log(`Connected correctly to server`);
 
-  console.log(`Connected correctly to server`);
-
-  const db = client.db(dbname);
-  dboper.insertDocument(
-    db,
-    { name: "Vadonut", description: "Test" },
-    "dishes",
-    (result) => {
-      console.log("Insert Document:\n", result.ops);
-      dboper.findDocument(db, "dishes", (docs) => {
+    const db = client.db(dbname);
+    dboper
+      .insertDocument(db, { name: "Vadonut", description: "Test" }, "dishes")
+      .then((result) => {
+        console.log("Insert Document:\n", result.ops);
+        return dboper.findDocument(db, "dishes");
+      })
+      .then((docs) => {
         console.log("Found Document:\n", docs);
-        dboper.updateDocument(
+        return dboper.updateDocument(
           db,
           { name: "Vadonut" },
           { description: "Updated Test" },
-          "dishes",
-          (result) => {
-            console.log("Updated Document:\n", result.result);
-            dboper.findDocument(db, "dishes", (docs) => {
-              console.log("Found Document:\n", docs);
-              db.dropCollection("dishes", (result) => {
-                console.log("Dropped Collection: ", result);
-                client.close();
-              });
-            });
-          }
+          "dishes"
         );
+      })
+      .then((result) => {
+        console.log("Updated Document:\n", result.result);
+        return dboper.findDocument(db, "dishes");
+      })
+      .then((docs) => {
+        console.log("Found Document:\n", docs);
+        return db
+          .dropCollection("dishes")
+          .then((result) => {
+            console.log("Dropped Collection: ", result);
+            client.close();
+          })
+          .catch((err) => console.log(err));
       });
-    }
-  );
-});
+  })
+  .catch((err) => console.log(err));
