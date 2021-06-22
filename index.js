e270970cb92fa5b447afca61694774ae3fe9d922
GDP-IrvanAriyanto <irvan.ariyanto@gdplabs.id>
2021-06-22 13:32:06 +0700

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
+const dboper = require("./operations");
 
 const url = "mongodb://root:rootpassword@localhost:27017/";
 
 const dbname = "conFusion";
 
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
   assert.equal(err, null);
 
   console.log(`Connected correctly to server`);
 
   const db = client.db(dbname);
-  const collection = db.collection("dishes");
-
-  collection.insertOne(
-    { name: "Uthapizza", description: "test" },
-    (err, result) => {
-      assert.equal(err, null);
-
-      console.log("After Insert:\n");
-      console.log(result.ops);
-
-      collection.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-
-        console.log("Found:\n");
-        console.log(docs);
-
-        db.dropCollection("dishes", (err, result) => {
-          assert.equal(err, null);
-          client.close();
-        });
+  dboper.insertDocument(
+    db,
+    { name: "Vadonut", description: "Test" },
+    "dishes",
+    (result) => {
+      console.log("Insert Document:\n", result.ops);
+      dboper.findDocument(db, "dishes", (docs) => {
+        console.log("Found Document:\n", docs);
+        dboper.updateDocument(
+          db,
+          { name: "Vadonut" },
+          { description: "Updated Test" },
+          "dishes",
+          (result) => {
+            console.log("Updated Document:\n", result.result);
+            dboper.findDocument(db, "dishes", (docs) => {
+              console.log("Found Document:\n", docs);
+              db.dropCollection("dishes", (result) => {
+                console.log("Dropped Collection: ", result);
+                client.close();
+              });
+            });
+          }
+        );
       });
     }
   );
