73418623e72a3218c6fa4bb05ae4df53be915314
GDP-IrvanAriyanto <irvan.ariyanto@gdplabs.id>
2021-07-03 20:05:19 +0700

    Mongoose Part 1

---
+++ index.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const Dishes = require("./models/dishes");
+
+const url =
+  "mongodb://root:rootpassword@localhost:27017/conFusion?authSource=conFusion&w=1')";
+
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connect.then((db) => {
+  console.log("Connected correctly to server");
+
+  var newDish = Dishes({
+    name: "Uthapisszza",
+    description: "test",
+  });
+
+  newDish
+    .save()
+    .then((dish) => {
+      console.log(dish);
+      Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+      console.log(dishes);
+      return Dishes.remove({});
+    })
+    .then(() => {
+      return mongoose.connections.close();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
