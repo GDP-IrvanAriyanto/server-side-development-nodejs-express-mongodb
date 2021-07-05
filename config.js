10609ee8e198dba3bcc910ec09669e24814c97db
GDP-IrvanAriyanto <irvan.ariyanto@gdplabs.id>
2021-07-05 11:08:26 +0700

    OAuth with Passport and Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "214124-1231241-241413-413414",
   mongoUrl: "mongodb://root:rootpassword@localhost:27017/",
+  facebook: {
+    clientId: "",
+    clientSecret: "",
+  },
 };
